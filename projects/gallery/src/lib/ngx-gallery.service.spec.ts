b946d5f18a44aa66f0190fa85a073325b8aff7c7
Andy <a.kolkov@gmail.com>
2019-11-24 03:40:20 +0300

    Fix service test

--- projects/gallery/src/lib/ngx-gallery.service.spec.ts
+++ projects/gallery/src/lib/ngx-gallery.service.spec.ts
@@ -1,16 +1,17 @@
-import { TestBed } from '@angular/core/testing';
+import {inject, TestBed} from '@angular/core/testing';
 
 import { NgxGalleryService } from './ngx-gallery.service';
+import {HttpClientModule} from '@angular/common/http';
 
 describe('NgxGalleryService', () => {
-  let service: NgxGalleryService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(NgxGalleryService);
+ beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ HttpClientModule ],
+      providers: [NgxGalleryService]
+    });
   });
 
-  it('should be created', () => {
+ it('should be created', inject([NgxGalleryService], (service: NgxGalleryService) => {
     expect(service).toBeTruthy();
-  });
+  }));
 });
