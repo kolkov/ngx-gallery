b424fabd8d256f9c6aef0033cdfdf3f4f2c08295
Andy <a.kolkov@gmail.com>
2019-11-26 16:18:56 +0300

    ci(karma): Fix coverage report no output

--- projects/gallery/karma.conf.js
+++ projects/gallery/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../../coverage/gallery'),
+      dir: require('path').join(__dirname, '../../coverage/gallery'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
@@ -26,6 +26,12 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
