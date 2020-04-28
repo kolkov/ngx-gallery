5a2c2cc62f384d0c9419c8a58ff701946fc7eff0
Andy <a.kolkov@gmail.com>
2020-04-28 19:22:42 +0300

    fix(gallery): Debris removed

--- projects/gallery-app/src/app/app.component.ts
+++ projects/gallery-app/src/app/app.component.ts
@@ -22,7 +22,8 @@ export class AppComponent implements OnInit{
         thumbnailsColumns: 4,
         arrowPrevIcon: 'fa fa-chevron-left',
         arrowNextIcon: 'fa fa-chevron-right',
-        imageAnimation: NgxGalleryAnimation.Slide
+        imageAnimation: NgxGalleryAnimation.Slide,
+        imageSwipe: true
       },
       // max-width 800
       {
@@ -32,12 +33,14 @@ export class AppComponent implements OnInit{
         imagePercent: 80,
         thumbnailsPercent: 20,
         thumbnailsMargin: 20,
-        thumbnailMargin: 20
+        thumbnailMargin: 20,
+        imageSwipe: true
       },
       // max-width 400
       {
         breakpoint: 400,
-        preview: false
+        preview: false,
+        imageSwipe: true
       }
     ];
 
