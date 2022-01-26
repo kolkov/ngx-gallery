6efaf185aaf75535051c5e505b65218975b30e78
andrejm7 <cidomarcon@gmail.com>
2022-01-25 22:57:46 -0300

    Update ngx-gallery.module.ts
    Update for angular 13. Remove import brower

--- projects/gallery/src/lib/ngx-gallery.module.ts
+++ projects/gallery/src/lib/ngx-gallery.module.ts
@@ -8,7 +8,6 @@ import {NgxGalleryActionComponent} from './ngx-gallery-action/ngx-gallery-action
 import {NgxGalleryBulletsComponent} from './ngx-gallery-bullets/ngx-gallery-bullets.component';
 import {CommonModule} from '@angular/common';
 import {HAMMER_GESTURE_CONFIG, HammerGestureConfig} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @Injectable()
 export class CustomHammerConfig extends HammerGestureConfig  {
@@ -29,7 +28,7 @@ export class CustomHammerConfig extends HammerGestureConfig  {
     NgxGalleryActionComponent,
     NgxGalleryBulletsComponent
   ],
-  imports: [ CommonModule, BrowserAnimationsModule ],
+  imports: [ CommonModule ],
   exports: [NgxGalleryComponent],
   providers: [
     { provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig }
